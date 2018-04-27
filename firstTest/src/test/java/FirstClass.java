3ec092f83e6c3f1a549ac6e04341b16d79d9f3ca
Alexandra1612 <saschka161284@gmail.com>
2018-04-27 12:04:07 +0300

    created first test

---
+++ firstTest/src/test/java/FirstClass.java
@@ -0,0 +1,54 @@
+
+
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FirstClass {
+  private WebDriver driver;
+
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void searchWebDriverInChrome() throws Exception {
+    driver.get("https://www.katalon.com/resources-center/blog/katalon-automation-recorder/");
+    driver.findElement(By.xpath("//div[@id='blog-view']/div[2]")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+
+  }
+
